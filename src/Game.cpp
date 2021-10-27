3311502dc78abfb927cc6cdac8e25b4e95186b81
Michael R Ryan <c00239510@itcarlow.ie>
2021-10-27 18:03:29 +0100

    Added the rest of the inputs

--- src/Game.cpp
+++ src/Game.cpp
@@ -91,222 +91,156 @@ void Game::processEvents()
                 //DEBUG_MSG("ge::Events::Event::ATTACK_START");
                 event.setCurrent(ge::Events::Event::ATTACK_START_EVENT);
             }
+            //Attack
+            else if (input.second.count(SDLK_z) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::ATTACK_START_EVENT");
+                event.setCurrent(ge::Events::Event::ATTACK_START_EVENT);
+            }
+            // Throw attack
+            else if (input.second.count(SDLK_x) > 0 && input.second.count(SDLK_RIGHT) > 0) 
+            {
+                //DEBUG_MSG("ge::Events::Event::THROW_START_EVENT");
+                event.setCurrent(ge::Events::Event::THROW_START_EVENT);
+            }
+            // Throw Attack
+            else if (input.second.count(SDLK_x) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::THROW_START_EVENT");
+                event.setCurrent(ge::Events::Event::THROW_START_EVENT);
+            }
+            // Run Right
+            else if (input.second.count(SDLK_RIGHT) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::RUN_RIGHT_START_EVENT");
+                event.setCurrent(ge::Events::Event::RUN_RIGHT_START_EVENT);
+            }
+            // Climb Up
+            else if (input.second.count(SDLK_UP) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::MOVE_UP_START_EVENT");
+                event.setCurrent(ge::Events::Event::MOVE_UP_START_EVENT);
+            }
+            // Climb Down
+            else if (input.second.count(SDLK_DOWN) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::MOVE_DOWN_START_EVENT");
+                event.setCurrent(ge::Events::Event::MOVE_DOWN_START_EVENT);
+            }
+            // Hit Bottom of Ladder Event
+            else if (input.second.count(SDLK_c) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::HIT_LADDER_BOTTOM_EVENT");
+                event.setCurrent(ge::Events::Event::HIT_LADDER_BOTTOM_EVENT);
+            }
+            // Hit Top of Ladder Event
+            else if (input.second.count(SDLK_t) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::HIT_LADDER_TOP_EVENT");
+                event.setCurrent(ge::Events::Event::HIT_LADDER_TOP_EVENT);
+            }
+            // Jump Run
+            if (input.second.count(SDLK_SPACE) > 0 && input.second.count(SDLK_RIGHT) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::JUMP_UP_EVENT");
+                event.setCurrent(ge::Events::Event::JUMP_UP_EVENT);
+            }
+            // Jump Event
+            else if (input.second.count(SDLK_SPACE) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::JUMP_UP_EVENT");
+                event.setCurrent(ge::Events::Event::JUMP_UP_EVENT);
+            }
+            // Running Slide
+            else if (input.second.count(SDLK_DOWN) > 0 && input.second.count(SDLK_RIGHT) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::SLIDE_EVENT");
+                event.setCurrent(ge::Events::Event::SLIDE_EVENT);
+            }
+            // Hit Ground Event
+            else if (input.second.count(SDLK_h) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::HIT_GROUND_EVENT");
+                event.setCurrent(ge::Events::Event::HIT_GROUND_EVENT);
+            }
+
+            // Jump Attack Event
+            else if (input.second.count(SDLK_h) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::HIT_GROUND_EVENT");
+                event.setCurrent(ge::Events::Event::HIT_GROUND_EVENT);
+            }
+
+            // Jump Throw Attack Event
+            else if (input.second.count(SDLK_h) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::HIT_GROUND_EVENT");
+                event.setCurrent(ge::Events::Event::HIT_GROUND_EVENT);
+            }
 
             break;
         case SDL_KEYUP:
+
+            // Run and Stop Attack
+            if (input.second.count(SDLK_z) > 0 && input.second.count(SDLK_RIGHT) > 0) // If z is released and right is down.
+            {
+                //DEBUG_MSG("ge::Events::Event::RUN_RIGHT_START_EVENT");
+                event.setCurrent(ge::Events::Event::RUN_RIGHT_START_EVENT);
+            }
+            // Stop Attack
+            else if(input.second.count(SDLK_z) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::ATTACK_STOP_EVENT");
+                event.setCurrent(ge::Events::Event::ATTACK_STOP_EVENT);
+            }
+            // Run and Stop Throw Attack
+            else if (input.second.count(SDLK_z) > 0 && input.second.count(SDLK_RIGHT) > 0) // If x is released and right is down.
+            {
+                //DEBUG_MSG("ge::Events::Event::RUN_RIGHT_START_EVENT");
+                event.setCurrent(ge::Events::Event::RUN_RIGHT_START_EVENT);
+            }
+            // Stop Throw Attack
+            else if (input.second.count(SDLK_x) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::THROW_STOP_EVENT");
+                event.setCurrent(ge::Events::Event::THROW_STOP_EVENT);
+            }
+            // Stop Running Right
+            else if (input.second.count(SDLK_RIGHT) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::RUN_RIGHT_STOP_EVENT");
+                event.setCurrent(ge::Events::Event::RUN_RIGHT_STOP_EVENT);
+            }
+            // Stop Slide
+            else if (input.second.count(SDLK_DOWN) > 0 && input.second.count(SDLK_RIGHT) > 0) // If down released and right is down.
+            {
+                //DEBUG_MSG("ge::Events::Event::RUN_RIGHT_START_EVENT");
+                event.setCurrent(ge::Events::Event::RUN_RIGHT_START_EVENT);
+            }
+            // Stop Moving Up
+            else if (input.second.count(SDLK_UP) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::MOVE_UP_STOP_EVENT");
+                event.setCurrent(ge::Events::Event::MOVE_UP_STOP_EVENT);
+            }
+            // Stop Moving Down
+            else if (input.second.count(SDLK_DOWN) > 0)
+            {
+                //DEBUG_MSG("ge::Events::Event::MOVE_DOWN_STOP_EVENT");
+                event.setCurrent(ge::Events::Event::MOVE_DOWN_STOP_EVENT);
+            }
+
             break;
         
         default:
+            //DEBUG_MSG("ge::Events::Event::NONE");
+            event.setCurrent(ge::Events::Event::NONE);
             break;
         }
     }
 
     // Handle input to Player
     m_player->handleInput(event);
-
-    // switch (event.type)
-    // {
-    // case sf::Event::Closed:
-    //     // Close window : exit
-    //     window.close();
-    //     break;
-    //     // Deal with KeyPressed
-    // case sf::Event::KeyPressed:
-    //     // Died Event
-    //     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-    //         DEBUG_MSG("gpp::Events::Event::DIED_EVENT");
-    //         input.setCurrent(gpp::Events::Event::DIED_EVENT);
-    //     }
-    //     // Revieved Event
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::R)) {
-    //         DEBUG_MSG("gpp::Events::Event::REVIVED_EVENT");
-    //         input.setCurrent(gpp::Events::Event::REVIVED_EVENT);
-    //     }
-    //     // Running attack
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z) 
-    //         && 
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::Right) 
-    //         ||
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::Right) 
-    //         && 
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::Z)
-    //         )
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::ATTACK_START");
-    //         input.setCurrent(gpp::Events::Event::ATTACK_START_EVENT);
-    //     }
-    //     // Attack
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::ATTACK_START_EVENT");
-    //         input.setCurrent(gpp::Events::Event::ATTACK_START_EVENT);
-    //     }
-    //     // Throw attack
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::X) 
-    //         && 
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::Right)
-    //         ||
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::Right) 
-    //         && 
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::X)
-    //         )
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::THROW_START_EVENT");
-    //         input.setCurrent(gpp::Events::Event::THROW_START_EVENT);
-    //     }
-    //     // Throw Attack
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::X))
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::THROW_START_EVENT");
-    //         input.setCurrent(gpp::Events::Event::THROW_START_EVENT);
-    //     }
-    //     // Run Right
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::RUN_RIGHT_START_EVENT");
-    //         input.setCurrent(gpp::Events::Event::RUN_RIGHT_START_EVENT);
-    //     }
-    //     // Climb Up
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::MOVE_UP_START_EVENT");
-    //         input.setCurrent(gpp::Events::Event::MOVE_UP_START_EVENT);
-    //     }
-    //     // Climb Down
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::MOVE_DOWN_START_EVENT");
-    //         input.setCurrent(gpp::Events::Event::MOVE_DOWN_START_EVENT);
-    //     }
-    //     // Hit Bottom of Ladder Event
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::C))
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::HIT_LADDER_BOTTOM_EVENT");
-    //         input.setCurrent(gpp::Events::Event::HIT_LADDER_BOTTOM_EVENT);
-    //     }
-    //     // Hit Top of Ladder Event
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::T))
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::HIT_LADDER_TOP_EVENT");
-    //         input.setCurrent(gpp::Events::Event::HIT_LADDER_TOP_EVENT);
-    //     }
-    //     // Jump Run
-    //     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space) 
-    //         && 
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::Right)
-    //         ||
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::Right) 
-    //         && 
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::Space)
-    //         )
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::JUMP_UP_EVENT");
-    //         input.setCurrent(gpp::Events::Event::JUMP_UP_EVENT);
-    //     }
-    //     // Jump Event
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::JUMP_UP_EVENT");
-    //         input.setCurrent(gpp::Events::Event::JUMP_UP_EVENT);
-    //     }
-    //     // Running Slide
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) 
-    //         && 
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::Right)
-    //         ||
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::Right) 
-    //         && 
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::Down)
-    //         )
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::SLIDE_EVENT");
-    //         input.setCurrent(gpp::Events::Event::SLIDE_EVENT);
-    //     }
-    //     // Hit Ground Event
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::H))
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::HIT_GROUND_EVENT");
-    //         input.setCurrent(gpp::Events::Event::HIT_GROUND_EVENT);
-    //     }
-
-    //     // Jump Attack Event
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::H))
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::HIT_GROUND_EVENT");
-    //         input.setCurrent(gpp::Events::Event::HIT_GROUND_EVENT);
-    //     }
-
-    //     // Jump Throw Attack Event
-    //     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::H))
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::HIT_GROUND_EVENT");
-    //         input.setCurrent(gpp::Events::Event::HIT_GROUND_EVENT);
-    //     }
-    //     break;
-
-    //     // Deal with KeyReleased
-    // case sf::Event::KeyReleased:
-    //     // Run and Stop Attack
-    //     if (event.key.code == sf::Keyboard::Z 
-    //         && 
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::RUN_RIGHT_START_EVENT");
-    //         input.setCurrent(gpp::Events::Event::RUN_RIGHT_START_EVENT);
-    //     }
-    //     // Stop Attack
-    //     else if(event.key.code == sf::Keyboard::Z)
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::ATTACK_STOP_EVENT");
-    //         input.setCurrent(gpp::Events::Event::ATTACK_STOP_EVENT);
-    //     }
-    //     // Run and Stop Throw Attack
-    //     else if (event.key.code == sf::Keyboard::X
-    //         &&
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::RUN_RIGHT_START_EVENT");
-    //         input.setCurrent(gpp::Events::Event::RUN_RIGHT_START_EVENT);
-    //     }
-    //     // Stop Throw Attack
-    //     else if (event.key.code == sf::Keyboard::X)
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::THROW_STOP_EVENT");
-    //         input.setCurrent(gpp::Events::Event::THROW_STOP_EVENT);
-    //     }
-    //     // Stop Running Right
-    //     else if (event.key.code == sf::Keyboard::Right)
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::RUN_RIGHT_STOP_EVENT");
-    //         input.setCurrent(gpp::Events::Event::RUN_RIGHT_STOP_EVENT);
-    //     }
-    //     // Stop Slide
-    //     else if (event.key.code == sf::Keyboard::Down 
-    //         &&
-    //         sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::RUN_RIGHT_START_EVENT");
-    //         input.setCurrent(gpp::Events::Event::RUN_RIGHT_START_EVENT);
-    //     }
-    //     // Stop Moving Up
-    //     else if (event.key.code == sf::Keyboard::Up)
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::MOVE_UP_STOP_EVENT");
-    //         input.setCurrent(gpp::Events::Event::MOVE_UP_STOP_EVENT);
-    //     }
-    //     // Stop Moving Down
-    //     else if (event.key.code == sf::Keyboard::Down)
-    //     {
-    //         DEBUG_MSG("gpp::Events::Event::MOVE_DOWN_STOP_EVENT");
-    //         input.setCurrent(gpp::Events::Event::MOVE_DOWN_STOP_EVENT);
-    //     }
-    //     break;
-
-    // default:
-    //     DEBUG_MSG("gpp::Events::Event::NONE");
-    //     input.setCurrent(gpp::Events::Event::NONE);
-    //     break;
-    // }
 }
 
 void Game::update()
